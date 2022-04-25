f3600eea01780c0b5cf089cbec49537c97249098
claudia <claudinipaz2@gmail.com>
2022-04-25 12:05:57 -0300

    prueba2

---
+++ lest.js
@@ -0,0 +1,13 @@
+alert("HOLA MUNDO")
+let i = 1;
+while (i < 10) 
+{
+  console.log(i);
+   i++; 
+}
+
+let nombre = "Claudia";
+let edad = "30";
+
+let concatenacion = nombre +" "+ edad;
+document.write(concatenacion);
